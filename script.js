380f5f4dd60b8ec6602160fd61b9a63066361500
Carlos <carlos50957580@gmail.com>
2024-06-06 09:39:37 -0400

    consumiendo la api y mostrando los usuarios por consola

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const apiurl = 'https://jsonplaceholder.typicode.com/users'
+const usersContainer = document.getElementById('users_div');
+
+
+async function getUsers() {
+
+        const response = await fetch(apiurl);
+        const data = await response.json();
+        displayUsers(data.results);
+        console.log(data);
+}
+  
+
+function displayUsers(users) {
+
+
+}
+getUsers(); 
