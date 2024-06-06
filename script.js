2a1d4f43b388fc0db02e95d437303f199002abc6
Carlos <carlos50957580@gmail.com>
2024-06-06 10:54:57 -0400

    mejorar la logica de las funciones displayUsers y getUsers

--- script.js
+++ script.js
@@ -6,13 +6,32 @@ async function getUsers() {
 
         const response = await fetch(apiurl);
         const data = await response.json();
-        displayUsers(data.results);
+        displayUsers(data);
         console.log(data);
 }
   
 
 function displayUsers(users) {
+   /* usersContainer.innerHTML = "";
+    users.forEach(user => {
+        const usersDiv = document.createElement('div');
+        usersDiv.classList.add('col-md-4');
+        usersDiv.innerHTML = `
+        <p>nombre: ${users.name}</p>
 
+        <div class="card m-5" style="width: 23rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text ">Nombre: </p>
+          <p class="card-text">Apellido: </p>
+          <p class="card-text">Dirrecion: </p>
+          <p class="card-text">email: </p>
+          <a href="#" class="btn btn-primary">ver arbunes </a>
+          <a href="#" class="btn btn-primary">ver post </a>
+          <a href="#" class="btn btn-primary">ver quehaceres </a>
 
-}
+        `;
+        usersContainer.appendChild(usersDiv);*/
+    }
+
 getUsers(); 
