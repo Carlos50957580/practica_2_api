db78032c40076dca108f13298322bba25c2aad56
Carlos <carlos50957580@gmail.com>
2024-06-07 11:00:47 -0400

    Uso de cartas con bootstrap por teminal

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiurl = 'https://jsonplaceholder.typicode.com/users'
+const apiurl = 'https://jsonplaceholder.typicode.com/users';
 const usersContainer = document.getElementById('users_div');
 
 
@@ -12,26 +12,32 @@ async function getUsers() {
   
 
 function displayUsers(users) {
-   /* usersContainer.innerHTML = "";
+   usersContainer.innerHTML = "";
     users.forEach(user => {
         const usersDiv = document.createElement('div');
-        usersDiv.classList.add('col-md-4');
+        usersDiv.classList.add('card', 'col-md-4');
         usersDiv.innerHTML = `
-        <p>nombre: ${users.name}</p>
 
-        <div class="card m-5" style="width: 23rem;">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text ">Nombre: </p>
-          <p class="card-text">Apellido: </p>
-          <p class="card-text">Dirrecion: </p>
-          <p class="card-text">email: </p>
+      <div class="card-body">
+        <h5 class="card-title">Nombre: ${user.name}</h5>
+          <h5 class="card-title">Email: ${user.email}</h5>
+          <h5 class="card-title">Dirreccion: ${user.address}</h5>
+          <h5 class="card-title">Numero de telefono: ${user.phone}</h5>
+          <h5 class="card-title">Website: ${user.website}</h5>
+
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
           <a href="#" class="btn btn-primary">ver arbunes </a>
           <a href="#" class="btn btn-primary">ver post </a>
           <a href="#" class="btn btn-primary">ver quehaceres </a>
+        </div>
+    </div>
+
 
         `;
-        usersContainer.appendChild(usersDiv);*/
+        usersContainer.appendChild(usersDiv);
+      });
     }
 
 getUsers(); 
