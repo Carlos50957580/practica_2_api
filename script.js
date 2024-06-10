b4fdfc398c88f1a2ecba6c1784a550dc7da96fdc
Carlos <carlos50957580@gmail.com>
2024-06-10 15:48:20 -0400

    uso de bootstrap para las cartas terminado

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const usersContainer = document.getElementById('users_div');
 
 
 async function getUsers() {
-
+  
         const response = await fetch(apiurl);
         const data = await response.json();
         displayUsers(data);
@@ -12,27 +12,23 @@ async function getUsers() {
   
 
 function displayUsers(users) {
-   usersContainer.innerHTML = "";
-    users.forEach(user => {
-        const usersDiv = document.createElement('div');
-        usersDiv.classList.add('card', 'col-md-4');
-        usersDiv.innerHTML = `
-
-      <div class="card-body">
-        <h5 class="card-title">Nombre: ${user.name}</h5>
-          <h5 class="card-title">Email: ${user.email}</h5>
-          <h5 class="card-title">Dirreccion: ${user.address}</h5>
-          <h5 class="card-title">Numero de telefono: ${user.phone}</h5>
-          <h5 class="card-title">Website: ${user.website}</h5>
-
-          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-          <a href="#" class="btn btn-primary">ver arbunes </a>
-          <a href="#" class="btn btn-primary">ver post </a>
-          <a href="#" class="btn btn-primary">ver quehaceres </a>
-        </div>
-    </div>
+  usersContainer.innerHTML = "";
+  users.forEach(user => {
+      const usersDiv = document.createElement('div');
+      usersDiv.classList.add('col-md-4');
+      usersDiv.innerHTML = `
+          <div class="card h-100">
+              <div class="card-body">
+                  <h5 class="card-title">Nombre: ${user.name}</h5>
+                  <h5 class="card-title">Email: ${user.email}</h5>
+                  <h5 class="card-title">Dirección: ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</h5>
+                  <h5 class="card-title">Número de teléfono: ${user.phone}</h5>
+                  <h5 class="card-title">Website: ${user.website}</h5>
+                  <a href="#" class="btn btn-primary">Ver álbumes</a>
+                  <a href="#" class="btn btn-primary">Ver posts</a>
+                  <a href="#" class="btn btn-primary">Ver quehaceres</a>
+              </div>
+          </div>
 
 
         `;
